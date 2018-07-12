9d51cf2b9534e1f7fb23e536338986a2128d8531
winvis <winvissoftware@gmail.com>
2018-07-13 01:17:36 +0900

    fix

--- task.ps1
+++ task.ps1
@@ -1,20 +1,11 @@
 param($method)
 $ScriptPath =(Split-Path $MyInvocation.mycommand.path -Parent)
 $erroractionpreference = "stop"
-."${ScriptPath}\DBConnnect.ps1"
-$db = ((Resolve-Path ".\taskListDB.accdb").path)
-$con = getConnection $db
+."${ScriptPath}\DBController.ps1"
+$db = (join-Path $PSScriptRoot "taskListDB.db")
 ".\functions\*.ps1" | 
 Resolve-Path | 
 Where-Object{!$_.Path.ToLower().contains(".tests.") } |
 ForEach-Object{
     . $_.path
 }
-
-$isValid = ((Get-ChildItem function:).name).ToLower().Contains($method.ToLower())
-if ($isValid -eq $true){
-    & $method $args[0] $args[1] $args[2] $args[3] $args[4] $args[5] $args[6] $args[7] $args[8] $args[9]
-}else{
-    "$($args[0]) is invaild Command."
-}
-disposeConnection $con
